19b98bbb47b454d97bd533c026198d73a6d95a02
Marcus Rådell <marcus.radell@sveasolar.com>
2020-06-17 22:24:58 +0200

    change to sequenceT

--- src/modules/identity/index.ts
+++ src/modules/identity/index.ts
@@ -3,15 +3,12 @@ import { Express } from "express";
 import jwt from "jsonwebtoken";
 import { pipe } from "fp-ts/lib/pipeable";
 import { chain, right, either } from "fp-ts/lib/Either";
-import { sequenceS } from "fp-ts/lib/Apply";
+import { sequenceT } from "fp-ts/lib/Apply";
 
 export const create = (app: Express) =>
   pipe(
-    sequenceS(either)({
-      jwtSecret: env("JWT_SECRET"),
-      password: env("PASSWORD"),
-    }),
-    chain(({ password, jwtSecret }) => {
+    sequenceT(either)(env("JWT_SECRET"), env("PASSWORD")),
+    chain(([jwtSecret, password]) => {
       app.post("/api/identity", (req, res) => {
         if (req.body.password !== password) {
           return res.json({
