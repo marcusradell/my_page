3e945921d226a7ba3d363470354167fa9045b736
Marcus Rådell <marcus.radell@sveasolar.com>
2020-06-17 22:17:31 +0200

    create an instance of the identity module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import * as identity from "./modules/identity";
+import express from "express";
+import { fold } from "fp-ts/lib/Either";
 
 export const main = () => {
   dotenv.config();
+  const app = express();
+
+  const identityApi = identity.create(app);
+  fold(
+    (errors) => {
+      console.error(errors);
+    },
+    () => {
+      console.log("Identity module initialized.");
+    }
+  )(identityApi);
 };
