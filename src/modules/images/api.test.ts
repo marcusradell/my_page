271b800e03d418a5205ff29f72d0eec84bdedf24
Marcus Rådell <marcus.radell@sveasolar.com>
2020-08-08 21:29:31 +0200

    TCR @ Sat Aug  8 21:29:31 2020

--- src/modules/images/api.test.ts
+++ src/modules/images/api.test.ts
@@ -1,3 +1,36 @@
-test("it works", () => {
-  expect(0).toEqual(0);
+import { Result } from "frea-core";
+
+type LoginErr = {
+  reason: "invalid_request";
+  validations: {
+    username: "required";
+    password: "required";
+  };
+};
+
+type LoginOk = unknown;
+
+type Login = (args: {
+  nickname: string;
+  password: string;
+}) => Result<LoginOk, LoginErr>;
+
+const login: Login = async ({ nickname, password }) => ({
+  status: "failed",
+  error: {
+    reason: "invalid_request",
+    validations: { password: "required", username: "required" },
+  },
+});
+
+test.skip("Empty nickname and password", async () => {
+  const result = await login({ nickname: "", password: "" });
+
+  expect(result).toEqual({
+    status: "failed",
+    error: {
+      reason: "invalid_request",
+      validations: { username: "required", password: "required" },
+    },
+  });
 });
